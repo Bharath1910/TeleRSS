a40be9ed0748d4e350089df09686986c09339570
Bharath <av.bharathshanmugam@gmail.com>
2022-01-22 22:12:34 +0530

    Changed config settings

---
+++ config_template.py
@@ -0,0 +1,6 @@
+TOKEN="" # Telegram API Token
+DBN="" # Database name
+DBU="" # Database Username
+DBP="" # Database Password
+DBH="" # Database Host
+DBP="" # Database Port, its 5432 by default
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import telebot
 import psycopg2
 
-from config import TOKEN
-from config import DBP
-from config import DBU
+from config import TOKEN, DBU, DBU, DBP, DBH
 
 bot = telebot.TeleBot(
     token=TOKEN,
     parse_mode="markdown"
 )
 
-conn = psycopg2.connect(database="telerss", user=DBU, password=DBP, host="127.0.0.1")
+conn = psycopg2.connect(
+    database=DBN,
+    user=DBU,
+    password=DBP,
+    host=DBH,
+    port=DBP
+    )
+
 cur = conn.cursor()
 
 @bot.message_handler(commands=['start'])
@@ -18,11 +23,10 @@ def message_handler(message):
     bot.send_message(message.chat.id, "text....pls do '/add'")
 
 
-@bot.message_handler(content_types=['text'])
-def add_links(message):
-    if message.text.lower() == '/add':
-        msg = bot.send_message(message.chat.id, "text...please send rss link")
-        bot.register_next_step_handler(msg, handle_links)
+@bot.message_handler(commands=['add'])
+def message_handler(message):
+    msg = bot.send_message(message.chat.id, "text...please send rss link")
+    bot.register_next_step_handler(msg, handle_links)
 
 def handle_links(message):
     cur.execute("SELECT rss_links FROM links WHERE chat_id=%s", (message.chat.id, ))
