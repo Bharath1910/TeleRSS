f83d72ae0b3ee343b177b4e043a3138f418fd025
shaun276 <helloshaunak276@gmail.com>
2022-01-22 17:19:34 +0530

    update

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import telebot
-from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import TOKEN
 
@@ -8,40 +7,21 @@ bot = telebot.TeleBot(
     parse_mode="markdown"
 )
 
-def start_markup():
-    markup = InlineKeyboardMarkup()
-    markup.row_width = 1
-
-    markup.add(
-        InlineKeyboardButton("Send RSS links", callback_data="rss")
-    )
-
-    return markup
 
 @bot.message_handler(commands=['start'])
 def message_handler(message):
-    bot.send_message(message.chat.id, "Hi...blah blah blah", reply_markup=start_markup())
+    bot.send_message(message.chat.id, "text....pls do '/add'")
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    if call.data == "rss":
-        bot.send_message(call.message.chat.id, "Please send your RSS links one by one or seperate them with commas")
-        linkList = []
-        @bot.message_handler(content_types=["text"])
-        def handle_links(message):
-            if "," in message.text:
-                links = message.text.split(",")
-                for i in links:
-                    linkList.append(i.strip())
+@bot.message_handler(content_types=['text'])
+def add_links(message):
+    if message.text.lower() == '/add':
+        msg = bot.send_message(message.chat.id, "text...please send rss link")
+        bot.register_next_step_handler(msg, handle_links)
 
-            else:
-                linkList.append(str(message.text))
-
-            db = {str(message.chat.id): linkList}
-            # print(db)
-
-            # database handling: search for chat id; If not present, insert data to database else update
+def handle_links(message):
+    db = {str(message.chat.id): [f"{message.text}"]}
+    print(db)
 
 
 bot.infinity_polling()
