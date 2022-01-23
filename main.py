c1976ea172f8f5e1ac70a163cd2d4814f651f30d
Bharath <av.bharathshanmugam@gmail.com>
2022-01-23 12:45:09 +0530

    added RSS feed details

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from config import *
 import telebot, psycopg2
+import feedparser
 
 # Telegram API setup
 bot = telebot.TeleBot(
@@ -11,7 +12,7 @@ bot = telebot.TeleBot(
 conn = psycopg2.connect(
     database=DB_Name,user=DB_User,
     password=DB_Password,host=DB_Host,
-    port=DB_Port
+    port=DB_Port # Defaut port is 5432, use 5432 if you are not sure about it
     )
 
 cur = conn.cursor()
@@ -33,6 +34,7 @@ def handle_links(message):
 
 @bot.message_handler(commands=['start','help'])
 def message_handler(message):
+    # Doc strings looks nice tbh
     help_text = """
 Hey! This is a RSS Feed Bot.
 
@@ -46,6 +48,19 @@ Hey! This is a RSS Feed Bot.
 @bot.message_handler(commands=['add'])
 def message_handler(message):
     msg = bot.send_message(message.chat.id, "Send a RSS feed link.")
-    bot.register_next_step_handler(msg, handle_links)
+    bot.register_next_step_handler(msg, feed_details) 
+    # I changed the function from handle_links to feed_details, change it :sweat:
+
+# Accepts a string which should be a RSS feed,
+# then it parses the link and sends the
+# title, link and description of the feed.
+def feed_details(url):
+    link = feedparser.parse(url.text)
+    details =  {
+        "title":link.feed.title,
+        "link":link.feed.link,
+        "description":link.feed.description
+        }
+    bot.send_message(url.chat.id, f"Title: {details['title']}\nLink: {details['link']}\nDescription: {details['description']}")
 
 bot.infinity_polling()
